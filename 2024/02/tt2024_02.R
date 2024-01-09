176a746262eb969751a4ca0291577f9a5656ceba
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-01-09 17:01:01 -0600

    Create tt2024_02.R

---
+++ 2024/02/tt2024_02.R
@@ -0,0 +1,37 @@
+
+# --- TIDYTUESDAY::YYYY_WW --- #
+
+#https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
+
+# Packages ----
+
+# DATA ----
+
+## https://shorturl.at/
+
+# CLEAN ----
+
+# EXPLORE ----
+
+# VISUALISE ----
+
+# Raw ----
+
+# Rice ----
+
+# ANALYSE ----
+
+# Prompting ----
+
+# question
+# [...]
+# question
+# [...]
+# 
+# [---]
+## https://chat.openai.com/share/
+## https://g.co/bard/share/
+
+# COMMUNICATE ----
+
+# ... ----
