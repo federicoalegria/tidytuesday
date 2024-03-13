68aa78e2bddda0e996d223e5f68e8b4d1e682043
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-03-12 22:51:14 -0600

    create template.R

---
+++ template.R
@@ -0,0 +1,72 @@
+
+# --- TIDYTUESDAY::YYYY§WW --- #
+
+# https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
+
+# Load ----
+
+# packages
+pacman::p_load(
+  janitor,
+  skimr,
+  tidylog,
+  tidyverse
+)
+
+# data
+df <-
+  readr::read_csv(
+    'link.csv'
+  ) |>
+  clean_names()
+## https://shorturl.at/ :: dictionary (rich)
+## https://shorturl.at/ :: dictionary (raw)
+
+# Wrangle ----
+
+# eda ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# tokenizer
+# df |>
+#   unnest_tokens(output = word, 
+#                 input = variable) |>
+#   anti_join(stop_words, 
+#             by = "word") |>
+#   group_by(word) |>
+#   summarise(n = n()) |>
+#   arrange(desc(n))
+
+# Visualise ----
+
+# raw
+
+# rice
+
+# Analyse ----
+
+# unassisted
+
+# assisted
+
+## question
+## [...]
+## question
+## [...]
+## 
+## [---]
+### https://chat.openai.com/share/
+### https://g.co/bard/share/
+
+# Communicate ----
+
+# ...
