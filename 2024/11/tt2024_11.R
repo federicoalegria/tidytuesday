99ee15c2fbedad00f90a24e50c1222a773f73dc2
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-03-27 13:21:15 -0600

    update tt2024_11.R

--- 2024/11/tt2024_11.R
+++ 2024/11/tt2024_11.R
@@ -19,27 +19,24 @@ fsp <-
   readr::read_csv(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-03-12/fiscal_sponsor_directory.csv'
   ) |>
-  clean_names()
+  clean_names() |> 
+  rename(criteria = eligibility_criteria) |> 
+  rename(details = details_url) |>
+  rename(fee = fiscal_sponsorship_fee_description) |> 
+  rename(model = fiscal_sponsorship_model) |>
+  rename(types = project_types) |> 
+  rename(year_sponsor = year_fiscal_sponsor)
 ## https://shorturl.at/ajwDW :: dictionary (rich)
 ## https://shorturl.at/rBTY3 :: dictionary (raw)
 
 # Wrangle ----
 
-fsp |> 
-  rename(fee_description = fiscal_sponsorship_fee_description) |> 
-  rename(criteria = eligibility_criteria) |> 
-  rename(types = project_types) |> 
-  rename(sponsorship_model = fiscal_sponsorship_model) |> 
-  glimpse() |>
-  skim()
+# eda ----
 
 fsp |> 
-  rename(criteria = eligibility_criteria) |> 
   filter(str_detect(criteria, pattern = "programming")) |> 
   select(criteria) |> 
   str_view()
-
-# eda ----
 
 # names
 fsp |> 
@@ -67,9 +64,27 @@ fsp |>
 # https://r-graph-gallery.com/303-lollipop-plot-with-2-values.html
 # https://www.data-to-viz.com/graph/lollipop.html
 
-# xa "name"
-# ya "year"
-# dot year_00 & year_01
+fsp |>
+  arrange(desc(n_sponsored)) |>
+  slice(1:35) |>
+  select(name, year_501c3, year_sponsor) |>
+  ggplot(y = reorder(name, (name))) +
+  geom_segment(aes(
+    x = name,
+    xend = name,
+    y = year_501c3,
+    yend = year_sponsor
+  ), color = "grey") +
+  geom_point(aes(x = name, y = year_501c3),
+             color = rgb(0.2, 0.7, 0.1, 0.5),
+             size = 3) +
+  geom_point(aes(x = name, y = year_sponsor),
+             color = rgb(0.7, 0.2, 0.1, 0.5),
+             size = 3) +
+  coord_flip() +
+  theme(legend.position = "none") +
+  xlab("") +
+  ylab("")
 
 # Communicate ----
 
