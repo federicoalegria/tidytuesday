b9cb199c1f336b78bb2900bf4f73014f8a3a81be
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-03-28 18:44:36 -0600

    update tt2024_11.R

--- 2024/11/tt2024_11.R
+++ 2024/11/tt2024_11.R
@@ -7,6 +7,7 @@
 
 # packages
 pacman::p_load(
+  ggthemes,
   janitor,
   skimr,
   tidylog,
@@ -19,12 +20,15 @@ fsp <-
   readr::read_csv(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-03-12/fiscal_sponsor_directory.csv'
   ) |>
-  clean_names() |> 
-  rename(criteria = eligibility_criteria) |> 
+  clean_names() |>
+  mutate(name = factor(name, levels = rev(sort(unique(
+    name
+  ))))) |>
+  rename(criteria = eligibility_criteria) |>
   rename(details = details_url) |>
-  rename(fee = fiscal_sponsorship_fee_description) |> 
+  rename(fee = fiscal_sponsorship_fee_description) |>
   rename(model = fiscal_sponsorship_model) |>
-  rename(types = project_types) |> 
+  rename(types = project_types) |>
   rename(year_sponsor = year_fiscal_sponsor)
 ## https://shorturl.at/ajwDW :: dictionary (rich)
 ## https://shorturl.at/rBTY3 :: dictionary (raw)
@@ -32,11 +36,6 @@ fsp <-
 # Wrangle ----
 
 # eda ----
-
-fsp |> 
-  filter(str_detect(criteria, pattern = "programming")) |> 
-  select(criteria) |> 
-  str_view()
 
 # names
 fsp |> 
@@ -48,44 +47,65 @@ fsp |>
   glimpse() |>
   skim()
 
-# tokenizer
-fsp |>
-  unnest_tokens(output = word,
-                input = eligibility_criteria) |>
-  anti_join(stop_words,
-            by = "word") |>
-  group_by(word) |>
-  summarise(n = n()) |>
-  arrange(desc(n))
-
 # Visualise ----
 
 # Cleveland dotplot
-# https://r-graph-gallery.com/303-lollipop-plot-with-2-values.html
-# https://www.data-to-viz.com/graph/lollipop.html
 
 fsp |>
-  arrange(desc(n_sponsored)) |>
-  slice(1:35) |>
-  select(name, year_501c3, year_sponsor) |>
-  ggplot(y = reorder(name, (name))) +
+  filter(year_501c3 != year_sponsor) |>          # filter sponsors which waited over a year 
+  arrange(desc(n_sponsored)) |>                  # arrange for top sponsors
+  slice(1:25) |>                                 # top 25
+  select(name, year_501c3, year_sponsor) |>      # select variables of interest
+  ggplot(y = reorder(name, (name))) +            # sort names alphabetically
   geom_segment(aes(
     x = name,
     xend = name,
     y = year_501c3,
     yend = year_sponsor
-  ), color = "grey") +
-  geom_point(aes(x = name, y = year_501c3),
-             color = rgb(0.2, 0.7, 0.1, 0.5),
-             size = 3) +
-  geom_point(aes(x = name, y = year_sponsor),
-             color = rgb(0.7, 0.2, 0.1, 0.5),
-             size = 3) +
+  ),
+  color = "#3c3836") +
+  geom_point(
+    aes(x = name, y = year_501c3),
+    color = '#3c3836',
+    alpha = 0.9,
+    size = 3.5
+  ) +
+  geom_point(
+    aes(x = name, y = year_sponsor),
+    color = '#98971a',
+    alpha = 0.9,
+    size = 3.5
+  ) +
+  geom_text(
+    aes(x = name, y = year_501c3, label = year_501c3),
+    hjust = 1.5,
+    nudge_x = 0,
+    nudge_y = 0,
+    size = 3,
+    family = 'Consolas'
+  ) +
+  geom_text(
+    aes(x = name, y = year_sponsor, label = year_sponsor),
+    hjust = -0.5,
+    nudge_x = 0,
+    nudge_y = 0,
+    size = 3,
+    family = 'Consolas'
+  ) +
   coord_flip() +
-  theme(legend.position = "none") +
+  theme_minimal() +
+  theme(
+    legend.position = 'none',
+    text = element_text(family = 'Roboto Mono'),
+    plot.title = element_text(face = 'bold'),
+    plot.subtitle = element_text(family = 'Consolas'),
+    plot.caption = element_text(family = 'Consolas')
+  ) +
   xlab("") +
-  ylab("")
-
-# Communicate ----
+  ylab("") +
+  labs(title = "top fiscal sponsors",
+       subtitle = "comparison between 501(c)(3) affiliation year and first sponsorship year",
+       caption = "tidytuesday 2024§11〔https://shorturl.at/euxVW〕"
+)
 
 # ...
