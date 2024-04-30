fbc05e0ea21c14c50052b3f9cd5d86b2673cf4e0
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-04-29 23:04:33 -0600

    update tt2024_16.R

--- 2024/16/tt2024_16.R
+++ 2024/16/tt2024_16.R
@@ -8,6 +8,8 @@
 # packages ----
 pacman::p_load(
   data.table,                     # https://www.perplexity.ai/search/what-are-the-xqCDaaVNQ8SnVOzl77krAg
+  ggraph,
+  igraph,
   janitor,
   skimr,
   tidylog,
@@ -16,10 +18,10 @@ pacman::p_load(
 )
 
 # data ----
-d0 <-
-  fread(
-   'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-04-16/shiny_revdeps.csv' 
-  ) |> clean_names()
+# d0 <-
+#   fread(
+#    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-04-16/shiny_revdeps.csv' 
+#   ) |> clean_names()
 
 d1 <-
   fread(
@@ -42,50 +44,194 @@ d1 |>
   glimpse() |>
   skim()
 
+d1 |>
+  select(
+    author,
+    copyright,
+    date,
+    date_publication,
+    description,
+    license,
+    license_is_foss,
+    license_restricts_use,
+    note,
+    package
+  ) |>
+  glimpse() |>
+  skim()
+
 # tokenizer
-# df |>
-#   unnest_tokens(output = word, 
-#                 input = variable) |>
-#   anti_join(stop_words, 
-#             by = "word") |>
-#   group_by(word) |>
-#   summarise(n = n()) |>
-#   arrange(desc(n))
+d1 |>
+  select(
+    author,
+    copyright,
+    date,
+    date_publication,
+    description,
+    license,
+    license_is_foss,
+    license_restricts_use,
+    note,
+    package
+  ) |>
+  unnest_tokens(
+    output = word,
+    input = description
+  ) |>
+  anti_join(
+    stop_words,
+    by = "word"
+  ) |>
+  group_by(word) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
 
 # Visualise ----
 
-## https://r-graph-gallery.com/sankey-diagram.html
-## https://www.perplexity.ai/search/what-kind-of-1hNplpXKQAu7gMUjg1_pgg
+# bi-gram ----
 
-# raw
+d1 |>
+  select(author, description) |>
+  group_by(author) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  slice_max(n, n = 10) |>
+  left_join(d1, by = "author") |>
+  select(author, description) |> 
+  unnest_ngrams(word, description, n = 2) |> 
+  anti_join(stop_words, by = "word") |> 
+  group_by(word) |> 
+  summarise(n = n()) |> 
+  arrange(desc(n)
+  )
 
-# rice
+## bi-gram objects ----
 
-# Analyse ----
+d1_bigrams <- d1 |> 
+  select(author, description) |>
+  group_by(author) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  slice_max(n, n = 10) |>
+  left_join(d1, by = "author") |>
+  select(author, description) |> 
+  unnest_ngrams(word, description, n = 2) |> 
+  anti_join(stop_words, by = "word")
 
-# unassisted
+d1_bigrams_sep <- d1_bigrams |> 
+  separate(word, c("word1", "word2"), sep = " ")
 
-# assisted
+d1_bigrams_filtered <- d1_bigrams_sep |> 
+  filter(!word1 %in% stop_words$word) |> 
+  filter(!word2 %in% stop_words$word)
 
-## question
-## [...]
+d1_bigram_counts <- d1_bigrams_filtered |> 
+  count(word1, word2, sort = TRUE)
 
-### https://chat.openai.com/share/
-### https://g.co/bard/share/
+### graph ----
+d1_bigram_graph <- d1_bigram_counts |> 
+  filter(n > 3) |> 
+  igraph::graph_from_data_frame()
 
-# Communicate ----
+d1_bigram_graph
+
+set.seed(8080)
+
+ggraph(d1_bigram_graph, layout = "fr") +
+  geom_edge_link() +
+  geom_node_point(color = "#000000",
+                  alpha = 0.65,
+                  size = 2) +
+  geom_node_text(aes(label = name, family = "Consolas"), size = 3, vjust = 1, hjust = 1) +
+  labs(
+    title = "common bigrams in descriptions from top Shiny authors",
+    subtitle = "showing those that occurred more than three times and where neither word was a stop word",
+    caption = "."
+  ) +
+  theme(text = element_text(family = "Consolas"),
+        plot.title = element_text(face = "bold")
+  )
+
+a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+
+set.seed(8080)
+
+ggraph(d1_bigram_graph, layout = "fr") +
+  geom_edge_link(
+    aes(edge_alpha = n),
+    show.legend = FALSE,
+    arrow = a,
+    end_cap = circle(.07, 'inches')
+  ) +
+  geom_node_point(color = "#cc241d",
+                  alpha = 0.85,
+                  size = 3.5) +
+  geom_node_text(aes(label = name, family = "Consolas"), size = 3, vjust = 1, hjust = 1) +
+  labs(
+    title = "common bigrams in descriptions from top Shiny authors",
+    subtitle = "showing those that occurred more than three times and where neither word was a stop word",
+    caption = "."
+  ) +
+  theme(text = element_text(family = "Consolas"),
+        plot.title = element_text(face = "bold"),
+        panel.background = element_rect(fill = "#ffffff"),
+        plot.background = element_rect(fill = "#ffffff")
+  )
 
 # ...
 
-# head
+# top 10 authors
 d1 |> 
   group_by(author) |> 
   summarise(n = n()) |> 
   arrange(desc(n)) |> 
-  head()
+  head(10)
 
+# package names by top 10 authors
 d1 |> 
   filter(author == "Kartikeya Bolar") |> 
   select(package)
 
-# package names by top authors
+# descriptions by top 10 authors
+d1 |>
+  select(author, description) |>
+  group_by(author) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  slice_max(n, n = 10) |>
+  left_join(d1, by = "author") |>
+  select(author, description)
+## https://www.perplexity.ai/search/after-the-last-GV78wDlGRIuUiD8oEQ68pQ
+
+# description tokens by top 10 authors
+d1 |>
+  select(author, description) |>
+  group_by(author) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  slice_max(n, n = 10) |>
+  left_join(d1, by = "author") |>
+  select(author, description) |>
+  unnest_tokens(
+    output = word,
+    input = description
+  ) |>
+  anti_join(
+    stop_words,
+    by = "word"
+  ) |>
+  group_by(word) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
+
+# longest and shortest descriptions
+d1 |> 
+  select(description) |> 
+  arrange(desc(str_length(description)))
+## https://www.perplexity.ai/search/help-me-sort-e4z7LapTT8Ci7UNzQ9JTTg
+
+# shortest descriptions
+d1 |>
+  select(description) |>
+  arrange(desc(str_length(description))) |>
+  tail(25)
