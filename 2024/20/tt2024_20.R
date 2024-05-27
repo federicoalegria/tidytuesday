d2a04a90ec83342001a192b390f3512dbe0f4032
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-05-26 23:26:03 -0600

    update tt2024_20.R

--- 2024/20/tt2024_20.R
+++ 2024/20/tt2024_20.R
@@ -1,15 +1,16 @@
 # --- TIDYTUESDAY::2024§20  --- #
 
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2024/2024-05-14/readme.md
+# Do you think participants in this survey are representative of Americans in general?
 
 # Load ----
 
 # packages ----
 pacman::p_load(
   data.table,
+  ggstatsplot,
   janitor,
   skimr,
-  tidylog,
   tidyverse
 )
 
@@ -36,6 +37,13 @@ df |>
   glimpse() |>
   skim()
 
+# select ----
+
+df |> 
+  select(2:19) |> 
+  slice(28, 35, 50) |> 
+  glimpse()
+
 # Visualise ----
 
 # raw
@@ -44,14 +52,42 @@ df |>
 
 # Analyse ----
 
-# unassisted
+# differences ----
 
-# assisted
+# among caffeine preferences by political affiliation
+df_sp <- 
+  df |> 
+  select(strength, political_affiliation) |> 
+  mutate(
+    strength = case_when(
+      strength == "Somewhat light" ~ "light",
+      strength == "Weak" ~ "light",
+      strength == "Medium" ~ "medium",
+      strength == "Somewhat strong" ~ "strong",
+      strength == "Very strong" ~ "strong"
+    )
+  )
 
-### https://chat.openai.com/share/
-### https://gemini.google.com/app/
-### https://www.perplexity.ai/share/
+table(df_sp$strength, df_sp$political_affiliation)
+
+chisq.test(df_sp$strength, df_sp$political_affiliation)
+
+vcd::assocstats(table(df_sp$strength, df_sp$political_affiliation))
+
+ggbarstats(df_sp, 
+           x = strength, 
+           y = political_affiliation,
+           type = 'nonparametric')
+
+# favourite coffee drink among home and in-person workers
+# Cleaveland plot
+# tt2024_11
+
+df |> 
+  select(2:19)
 
 # Communicate ----
+
+# submission
 
 # ...
