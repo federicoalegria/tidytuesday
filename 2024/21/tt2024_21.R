a748de67f1ee1a21d28b4254099d7f793b1a42fa
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-05-29 22:54:14 -0600

    update tt2024_21.R

--- 2024/21/tt2024_21.R
+++ 2024/21/tt2024_21.R
@@ -7,6 +7,7 @@
 # packages ----
 pacman::p_load(
   data.table,
+  ggthemes,
   janitor,
   skimr,
   tidyverse
@@ -59,25 +60,54 @@ df |>
 
 # Visualise ----
 
-# raw
-
 df |> 
-  ggplot(aes(x = commodity,
-             y = total_emissions_mt_co2e)) +
-  geom_boxplot() +
-  facet_wrap(production_unit ~ .)
-
-# rice
-
-# Analyse ----
-
-# unassisted
-
-# assisted
-
-### https://chat.openai.com/share/
-### https://gemini.google.com/app/
-### https://www.perplexity.ai/share/
+  group_by(commodity) |>
+  summarize(
+    min_year = min(year),
+    max_year = max(year)
+  ) |>
+  arrange(desc(commodity)) |> 
+  ggplot() +
+  geom_segment(aes(
+    x = min_year,
+    xend = max_year,
+    y = commodity
+  ), color = "#c33027") +
+  geom_point(
+    aes(x = min_year, y = commodity),
+    color = '#c33027',
+    alpha = 0.9,
+    size = 3.5
+  ) +
+  geom_text(
+    aes(x = min_year, y = commodity, label = min_year),
+    size = 4,
+    hjust = -0.45,
+    vjust = -1.25,
+    family = 'Consolas',
+    color = '#d3ebe9'
+  ) +
+  theme_wsj(color = "#081F2D") +
+  theme(
+    axis.line.x = element_line(colour = "#d3ebe9"),
+    axis.text = element_text(family = 'Consolas', colour = "#d3ebe9"),
+    axis.ticks = element_line(colour = "#d3ebe9"),
+    legend.text = element_text(family = 'Consolas', colour = "#d3ebe9"),
+    legend.title = element_text(family = 'Consolas', size = 28, colour = "#d3ebe9"),
+    panel.background = element_rect(fill = "#081F2D", colour = "#081F2D"),
+    panel.grid.major.y = element_line(colour = "#d3ebe9", linetype = 'dotted'),
+    plot.background = element_rect(fill = "#081F2D", colour = "#081F2D"),
+    plot.caption = element_text(family = 'Consolas', size = 10, colour = "#d3ebe9"),
+    plot.subtitle = element_text(family = 'Consolas', size = 13, colour = "#d3ebe9"),
+    plot.title = element_text(size = 28, colour = "#d3ebe9"),
+    plot.title.position = 'plot',
+  ) +
+  xlab("") +
+  ylab("") +
+  labs(title = "commodities",
+       subtitle = " ",
+       caption = "tidytuesday 2024§21〔https://〕"
+)
 
 # Communicate ----
 
