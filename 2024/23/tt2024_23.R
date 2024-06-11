61c12d75626137f39461f408bc35492edfbe9523
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-06-10 21:39:07 -0600

    update tt2024_23.R

--- 2024/23/tt2024_23.R
+++ 2024/23/tt2024_23.R
@@ -6,7 +6,9 @@
 
 # packages ----
 pacman::p_load(
+  countrycode,
   data.table,
+  ggalluvial,
   janitor,
   skimr,
   tidyverse
@@ -37,14 +39,104 @@ df |>
 
 # Visualise ----
 
-# raw
+# sankey ----
+## https://corybrunson.github.io/ggalluvial/articles/ggalluvial.html
 
-# rice
+# ensure categorical axis
 
-# Analyse ----
+## continents
+custom_mapping <- c(
+  "England" = "Europe",
+  "Middle East" = "Asia",
+  "Scotland" = "Europe",
+  "Wales" = "Europe"
+)
 
-# ...
+df |>
+  mutate(country = str_extract(country, "^[^,]+")) |>
+  mutate(continent = recode(
+    country,
+    !!!custom_mapping,                                               # *
+    .default = countrycode(country, "country.name", "continent")
+    )
+  )
+## * splices the named vector so that each element is treated as an individual argument in the recode() function
+## https://rdrr.io/github/tidyverse/rlang/f/man/rmd/topic-metaprogramming.Rmd
+
+## continent_milk_rind
+df |>
+  mutate(country = str_extract(country, "^[^,]+")) |>
+  mutate(continent = recode(
+    country,!!!custom_mapping,
+    .default = countrycode(country, "country.name", "continent")
+  )) |>
+  mutate(milk = str_extract(milk, "^[^,]+")) |>
+  filter(!milk %in% c("cow", "plant-based")) |>
+  mutate(non_vegan = as.numeric(!vegan)) |>
+  drop_na(continent, milk, rind, non_vegan) |>
+  ggplot(aes(
+    axis1 = continent,
+    axis2 = milk,
+    axis3 = rind,
+    y = non_vegan
+  )) +
+  geom_alluvium(aes(fill = milk), alpha = 0.9, width = 1 / 12) +
+  geom_stratum(width = 1 / 12,
+               fill = "#d5ccba",
+               color = "#20111b") +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  scale_x_discrete(limits = c("Continent", "Milk", "Rind"),
+                   expand = c(0.15, 0.05)) +
+  scale_fill_manual(
+    values = c(
+      "buffalo" = "#be100e",
+      "camel" = "#5e5252",
+      "goat" = "#97522c",
+      "sheep" = "#ffd7b1",
+      "water buffalo" = "#426a79"
+    )
+  ) +
+  theme_minimal() +
+  labs(title = "alluvial view of cheese", x = " ", y = " ")
+
+## continent_milk_texture
+df |>
+  mutate(country = str_extract(country, "^[^,]+")) |>
+  mutate(continent = recode(
+    country,!!!custom_mapping,
+    .default = countrycode(country, "country.name", "continent")
+  )) |>
+  mutate(milk = str_extract(milk, "^[^,]+")) |>
+  mutate(texture = str_extract(texture, "^[^,]+")) |>
+  filter(!milk %in% c("cow", "plant-based")) |>
+  mutate(non_vegan = as.numeric(!vegan)) |>
+  drop_na(continent, milk, texture, non_vegan) |>
+  ggplot(aes(
+    axis1 = continent,
+    axis2 = milk,
+    axis3 = texture,
+    y = non_vegan
+  )) +
+  geom_alluvium(aes(fill = milk), alpha = 0.9, width = 1 / 12) +
+  geom_stratum(width = 1 / 12,
+               fill = "#d5ccba",
+               color = "#20111b") +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  scale_x_discrete(limits = c("continent", "milk", "texture"),
+                   expand = c(0.15, 0.05)) +
+  scale_fill_manual(
+    values = c(
+      "buffalo" = "#be100e",
+      "camel" = "#5e5252",
+      "goat" = "#97522c",
+      "sheep" = "#ffd7b1",
+      "water buffalo" = "#426a79"
+    )
+  ) +
+  theme_minimal() +
+  labs(title = "alluvial view of cheese", x = " ", y = " ")
 
 # Communicate ----
 
 # ...
+
