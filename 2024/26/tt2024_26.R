235b60f281fcffd77c5a1508d857ec6940d6ec65
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-06-26 23:38:34 -0600

    update tt2024_26.R

--- 2024/26/tt2024_26.R
+++ 2024/26/tt2024_26.R
@@ -9,7 +9,8 @@ pacman::p_load(
   data.table,
   janitor,
   skimr,
-  tidyverse
+  tidyverse,
+  tidytext              # https://juliasilge.github.io/tidytext/
 )
 
 # data ----
@@ -36,28 +37,9 @@ df |>
   glimpse() |> 
   skim()
 
-# tokenizer
-# df |>
-#   unnest_tokens(output = word, 
-#                 input = variable) |>
-#   anti_join(stop_words, 
-#             by = "word") |>
-#   group_by(word) |>
-#   summarise(n = n()) |>
-#   arrange(desc(n))
-
-# Visualise ----
-
-# raw
-
-plot(df$vote_average, df$popularity)
-plot(df$vote_average, df$vote_count)
-
-# rice
-
 # Analyse ----
 
-# logistic
+# logistic ----
 
 # numeric_explainer = vote_average
 # logical_response = adult
@@ -73,16 +55,18 @@ model <- glm(adult_binary ~ vote_average,
 
 summary(model)
 
-df |> 
-  mutate(adult_binary = if_else(adult, 1, 0)) |> 
-  select(vote_average, adult_binary) |> 
+df |>
+  mutate(adult_binary = if_else(adult, 1, 0)) |>
+  select(vote_average, adult_binary) |>
   ggplot(aes(x = vote_average,
              y = adult_binary)) +
   geom_jitter(alpha = .1,
-              height = .05) + 
-  geom_smooth(method = 'glm',
-              method.args = list(family = 'binomial'),
-              se = FALSE)
+              height = .05) +
+  geom_smooth(
+    method = 'glm',
+    method.args = list(family = 'binomial'),
+    se = FALSE
+)
 
 # numeric_explainer = vote_count
 # logical_response = adult
@@ -98,16 +82,18 @@ model <- glm(adult_binary ~ vote_count,
 
 summary(model)
 
-df |> 
-  mutate(adult_binary = if_else(adult, 1, 0)) |> 
-  select(vote_count, adult_binary) |> 
+df |>
+  mutate(adult_binary = if_else(adult, 1, 0)) |>
+  select(vote_count, adult_binary) |>
   ggplot(aes(x = vote_count,
              y = adult_binary)) +
   geom_jitter(alpha = .1,
-              height = .05) + 
-  geom_smooth(method = 'glm',
-              method.args = list(family = 'binomial'),
-              se = FALSE)
+              height = .05) +
+  geom_smooth(
+    method = 'glm',
+    method.args = list(family = 'binomial'),
+    se = FALSE
+)
 
 # numeric_explainer = popularity
 # logical_response = adult
@@ -123,17 +109,19 @@ model <- glm(adult_binary ~ popularity,
 
 summary(model)
 
-df |> 
-  mutate(adult_binary = if_else(adult, 1, 0)) |> 
-  select(popularity, adult_binary) |> 
-  filter(popularity <= 10) |> 
+df |>
+  mutate(adult_binary = if_else(adult, 1, 0)) |>
+  select(popularity, adult_binary) |>
+  filter(popularity <= 10) |>
   ggplot(aes(x = popularity,
              y = adult_binary)) +
   geom_jitter(alpha = .1,
-              height = .05) + 
-  geom_smooth(method = 'glm',
-              method.args = list(family = 'binomial'),
-              se = FALSE)
+              height = .05) +
+  geom_smooth(
+    method = 'glm',
+    method.args = list(family = 'binomial'),
+    se = FALSE
+)
 
 # numeric_explainer = video_binary
 # logical_response = adult
@@ -150,18 +138,74 @@ model <- glm(adult_binary ~ video_binary,
 
 summary(model)
 
-df |> 
-  mutate(adult_binary = if_else(adult, 1, 0)) |> 
-  mutate(video_binary = if_else(video, 1, 0)) |> 
-  select(adult_binary, video_binary) |> 
+df |>
+  mutate(adult_binary = if_else(adult, 1, 0)) |>
+  mutate(video_binary = if_else(video, 1, 0)) |>
+  select(adult_binary, video_binary) |>
   ggplot(aes(x = video_binary,
              y = adult_binary)) +
   geom_jitter(alpha = .1,
-              height = .05) + 
-  geom_smooth(method = 'lm',
-              method.args = list(family = 'binomial'),
-              se = FALSE)
+              height = .05) +
+  geom_smooth(
+    method = 'lm',
+    method.args = list(family = 'binomial'),
+    se = FALSE
+)
 
+# numeric_explainer = vote_average
+# logical_response = yay_nay_bin
+
+df_mod <-
+  df |>
+  select(vote_average) |>
+  mutate(
+    yay_nay = case_when(
+      vote_average > 7 ~ "yay",
+      vote_average < 6.99 ~ "nay",
+      TRUE ~ NA_character_
+    ),
+    yay_nay_bin = case_when(yay_nay == "yay" ~ 1,
+                            yay_nay == "nay" ~ 0,
+                            TRUE ~ NA_integer_)
+)
+
+model <- glm(yay_nay_bin ~ vote_average,
+             data = df_mod,
+             family = 'binomial')
+
+summary(model)
+
+df |>
+  select(vote_average) |>
+  mutate(
+    yay_nay = case_when(
+      vote_average > 7 ~ "yay",
+      vote_average < 6.99 ~ "nay",
+      TRUE ~ NA_character_
+    ),
+    yay_nay_bin = case_when(yay_nay == "yay" ~ 1,
+                            yay_nay == "nay" ~ 0,
+                            TRUE ~ NA_integer_)
+  ) |>
+  ggplot(aes(x = vote_average,
+             y = yay_nay_bin)) +
+  geom_jitter(alpha = .1,
+              height = .05) +
+  geom_smooth(
+    method = 'glm',
+    method.args = list(family = 'binomial'),
+    se = FALSE
+)
+
+# sentiment ----
+df |>
+  unnest_tokens(output = word,
+                input = overview) |>
+  anti_join(stop_words,
+            by = "word") |>
+  group_by(word) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
 
 # Communicate ----
 
