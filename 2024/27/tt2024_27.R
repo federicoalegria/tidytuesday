1d3a8f616d45c3081be671d9e07b5a53599dc29b
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-07-04 21:11:05 -0600

    update tt2024_27.R

--- 2024/27/tt2024_27.R
+++ 2024/27/tt2024_27.R
@@ -71,6 +71,50 @@ tt_data |>
 
 # raw
 
+tt_data |>
+  select(dataset_name, observations) |>
+  arrange(desc(observations)) |>
+  filter(observations >= 600000) |>
+  mutate(row_num = row_number()) |>
+  ggplot(aes(
+    x = 0,
+    xend = observations,
+    y = row_num,
+    yend = row_num
+  )) +
+  geom_hline(yintercept = 1:10,
+             size = 14,
+             color = "#dfdfdf") +
+  geom_hline(yintercept = 1:10,
+             size = 13,
+             color = "#f7f7f7") +
+  geom_segment(aes(color = dataset_name), size = 14, lineend = 'round') +
+  geom_segment(size = 10,
+               lineend = 'round',
+               color = "#f7f7f7") +
+  scale_color_manual(values = rep("#bf2c23", 10)) +
+  geom_point(
+    aes(x = observations - 0.03 * observations, color = dataset_name),
+    size = 5,
+    shape = 21,
+    fill = 'white'
+  ) +
+  geom_point(
+    aes(x = observations - 0.03 * observations, color = dataset_name),
+    size = 2,
+    shape = 21,
+    fill = 'white'
+  ) +
+  scale_y_continuous(
+    limits = c(0, 11),
+    breaks = 1:10,
+    labels = tt_data %>% arrange(desc(observations)) %>% filter(observations >= 600000) %>% pull(dataset_name)
+  ) +
+  scale_x_continuous(limits = c(0, max(tt_data$observations))) +
+  coord_polar() +
+  theme_void() +
+  theme(legend.position = 'none')
+
 # rice
 
 # Analyse ----
