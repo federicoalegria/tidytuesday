48a5b50b2619eb6d0c236ae84b3a82b1094dbc2f
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-07-05 17:37:51 -0600

    update tt2024_27.R

--- 2024/27/tt2024_27.R
+++ 2024/27/tt2024_27.R
@@ -14,22 +14,22 @@ pacman::p_load(
 
 # data ----
 tt_data <-
-  readr::read_csv(
+  fread(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_datasets.csv'
   ) |> clean_names()
 
 tt_summ <-
-  readr::read_csv(
+  fread(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_summary.csv'
   ) |> clean_names()
 
 tt_urls <-
-  readr::read_csv(
+  fread(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_urls.csv'
   ) |> clean_names()
 
 tt_vars <-
-  readr::read_csv(
+  fread(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-07-02/tt_variables.csv'
   ) |> clean_names()
 
@@ -73,47 +73,25 @@ tt_data |>
 
 tt_data |>
   select(dataset_name, observations) |>
+  mutate(observations = as.integer(observations)) |>
   arrange(desc(observations)) |>
   filter(observations >= 600000) |>
-  mutate(row_num = row_number()) |>
-  ggplot(aes(
-    x = 0,
-    xend = observations,
-    y = row_num,
-    yend = row_num
-  )) +
-  geom_hline(yintercept = 1:10,
-             size = 14,
-             color = "#dfdfdf") +
-  geom_hline(yintercept = 1:10,
-             size = 13,
-             color = "#f7f7f7") +
-  geom_segment(aes(color = dataset_name), size = 14, lineend = 'round') +
-  geom_segment(size = 10,
-               lineend = 'round',
-               color = "#f7f7f7") +
-  scale_color_manual(values = rep("#bf2c23", 10)) +
-  geom_point(
-    aes(x = observations - 0.03 * observations, color = dataset_name),
-    size = 5,
-    shape = 21,
-    fill = 'white'
+  mutate(dataset_name =
+           fct_reorder(dataset_name,
+                       dataset_name,
+                       .desc = TRUE)) |>
+  ggplot(aes(x = dataset_name,
+             y = observations)) +
+  geom_bar(stat = 'identity') +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    title = "",
+    subtitle = "",
+    x = "",
+    y = ""
   ) +
-  geom_point(
-    aes(x = observations - 0.03 * observations, color = dataset_name),
-    size = 2,
-    shape = 21,
-    fill = 'white'
-  ) +
-  scale_y_continuous(
-    limits = c(0, 11),
-    breaks = 1:10,
-    labels = tt_data %>% arrange(desc(observations)) %>% filter(observations >= 600000) %>% pull(dataset_name)
-  ) +
-  scale_x_continuous(limits = c(0, max(tt_data$observations))) +
-  coord_polar() +
-  theme_void() +
-  theme(legend.position = 'none')
+  coord_flip() +
+  ggthemes::theme_wsj()
 
 # rice
 
