42e22d4cd67288072c8af7d74afc7a05c873dbba
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-07-30 09:35:34 -0600

    update template.R

--- template.R
+++ template.R
@@ -1,19 +1,19 @@
 # --- TIDYTUESDAY::YYYY§WW --- #
 # https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
 
-# Load ----
-
+# library path
 .libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
 
-# packages ----
+# packages
 pacman::p_load(
   data.table,           # https://cran.r-project.org/web/packages/data.table/
   janitor,              # https://cran.r-project.org/web/packages/janitor/
   skimr,                # https://cran.r-project.org/web/packages/skimr/
+  styler,               # https://cran.r-project.org/web/packages/styler/
   tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
 )
 
-# data ----
+# Import ----
 df <-
   fread(
     'link.csv'
@@ -22,7 +22,7 @@ df <-
 # dictionary
 # https://raw.
 
-# Wrangle ----
+# Understand ----
 
 # names
 df |> 
@@ -34,10 +34,7 @@ df |>
   glimpse() |>
   skim()
 
-# eda ----
-
-# tokenize ----
-
+# tokenize
 # df |>
 #   unnest_tokens(output = word, input = variable) |>
 #   anti_join(stop_words, by = "word") |>
@@ -45,13 +42,14 @@ df |>
 #   summarise(n = n()) |>
 #   arrange(desc(n))
 
-# Visualise ----
+# transform ---
 
-# raw ----
-# rice ----
+# visualise ----
 
-# Analyse ----
+# raw
+# rice
+
+# model ----
 
 # Communicate ----
-
 # ...
