2a39d42cf9de0ebeb56dc2d8da3f0f389e6dc717
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-08-08 22:11:25 -0600

    update tt2024_32.R

--- 2024/32/tt2024_32.R
+++ 2024/32/tt2024_32.R
@@ -7,7 +7,10 @@
 # packages
 pacman::p_load(
   data.table,           # https://cran.r-project.org/web/packages/data.table/
+  ggforce,              # https://cran.r-project.org/web/packages/ggforce/
+  ggstatsplot,          # https://cran.r-project.org/web/packages/ggstatsplot/
   janitor,              # https://cran.r-project.org/web/packages/janitor/
+  nortest,              # https://cran.r-project.org/web/packages/nortest/
   skimr,                # https://cran.r-project.org/web/packages/skimr/
   styler,               # https://cran.r-project.org/web/packages/styler/
   tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
@@ -36,30 +39,51 @@ df |>
   glimpse() |>
   skim()
 
-# visualise ----
-
-# raw
-
-## violin plot
-df |> 
-  filter(!is.na(height) & !is.na(weight) & !is.na(medal)) |>         # filter na values
-  mutate(bmi = weight / ((height / 100) ^ 2)) |>     # calculate bmi
-  select(bmi, medal) |> 
-  ggplot(aes(x = medal, y = bmi)) +
-  geom_violin()
-
 # model ----
 
-## scatter plot
-df |> 
+df |>
   filter(sport == "Curling") |>
   ggplot(aes(x = height, y = weight, colour = sex)) +
-  geom_point() +
-  geom_smooth(method = 'lm') +
-  scale_color_brewer(palette = 'Accent')
+  geom_jitter(alpha = 0.35) +
+  geom_smooth(alpha = 0.7, method = 'lm') +
+  geom_mark_hull(aes(label = sex)) +
+  scale_color_manual(values = c("F" = "#D79921",
+                                "M" = "#B16286")) +
+  theme_minimal() +
+  theme(legend.position = 'none')
+
+# check for normality
+
+## visually
+df |>
+  filter(sport == "Curling") |>
+  ggplot(aes(sample = height)) +
+  stat_qq() +
+  stat_qq_line() +
+  theme_minimal() +
+  labs(title = "Q-Q Plot of Height for Curling Athletes",
+       x = "Theoretical Quantiles",
+       y = "Sample Quantiles")
+
+## analitically
+df |> 
+  filter(sport == "Curling") |>
+  pull(height) |>
+  ks.test("pnorm")
+
+# nonparametric way
+df |>
+  filter(sport == "Curling") |>
+  filter(!is.na(height) & !is.na(weight) & !is.na(medal)) |>
+  mutate(bmi = weight / ((height / 100) ^ 2)) |>
+  select(bmi, medal) |>
+  ggbetweenstats(x = medal,
+                 y = bmi,
+                 type = "nonparametric")
 
 # Communicate ----
 
+# does bmi influence the outcomes?
+
 # ...
 
-# does bmi influence the outcomes?
