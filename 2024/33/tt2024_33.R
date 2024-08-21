405dc932b031c466b1197f3440d6bbb2223e9eb8
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-08-21 16:05:38 -0600

    update tt2024_33.R

--- 2024/33/tt2024_33.R
+++ 2024/33/tt2024_33.R
@@ -7,6 +7,7 @@
 # packages
 pacman::p_load(
   data.table,           # https://cran.r-project.org/web/packages/data.table/
+  ggthemes,             # https://cran.r-project.org/web/packages/ggthemes/
   janitor,              # https://cran.r-project.org/web/packages/janitor/
   skimr,                # https://cran.r-project.org/web/packages/skimr/
   styler,               # https://cran.r-project.org/web/packages/styler/
@@ -57,8 +58,8 @@ df |>
 # fork & tweak
 # https://github.com/mitsuoxv/tidytuesday/blob/main/2024_08_13_worlds_fairs.qmd
 
-df |> 
-  filter(category == "World Expo", !is.na(visitors)) |> 
+df |>
+  filter(category == "World Expo", !is.na(visitors)) |>
   mutate(
     region = case_when(
       country %in% c("United States", "Canada") ~ "North America",
@@ -68,13 +69,34 @@ df |>
       .default = "Europe"
     ),
     region = factor(region, levels = c("Europe", "North America", "Oceania", "Middle East", "Far East"))
-  ) |> 
+  ) |>
+  filter(region != "Oceania") |> 
   ggplot(aes(visitors, factor(start_year))) +
   geom_col(aes(fill = region)) +
   geom_text(aes(label = city), vjust = 0.5, hjust = -0.1, size = 3) +
   scale_x_continuous(expand = expansion(add = c(0, 12))) +
-  labs(x = "Number of visitors (million)", y = "World Expo Year",
-       fill = "Region",
-       title = "Largest visitors in 2010 Shanghai Expo",
-       caption = "Source: List of world expositions (Wikipedia)") +
-  theme(panel.grid.major.y = element_blank())
+  labs(
+    x = "visitors in millions",
+    y = "world expo year",
+    fill = "region",
+    title = " ",
+    caption = " "
+  ) +
+  scale_fill_manual(
+    values = c(
+      "Europe" = '#fbbc04',
+      "North America" = '#4285f4',
+      "Middle East" = '#34a853',
+      "Far East" = '#ea4335'
+    )
+  ) +
+  theme_wsj() +
+  theme(
+    text = element_text(family = 'Roboto'),
+    axis.text.y = element_text(size = 8, family = 'Roboto'),
+    axis.title.y = element_text(size = 10, family = 'Roboto'),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 10, family = 'Roboto'),
+    plot.title = element_text(size = 14, family = 'Roboto'),
+    plot.caption = element_text(size = 10, family = 'Roboto')
+)
