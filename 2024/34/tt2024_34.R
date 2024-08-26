eb9dbcc9e7a3c2fc874cb92c9daccf3603f5b282
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-08-26 13:35:18 -0600

    update tt2024_34.R

--- 2024/34/tt2024_34.R
+++ 2024/34/tt2024_34.R
@@ -54,7 +54,8 @@ df |>
   mutate(consort_age = as.numeric(consort_age)) |> 
   select(king_age, consort_age) |> 
   mutate(age_diff = (king_age - consort_age)) |> 
-  filter(!is.na(age_diff))
+  filter(!is.na(age_diff)) |> 
+  arrange(desc(age_diff))
 
 # historical periods
 
@@ -74,6 +75,33 @@ df |>
       TRUE ~ "NA"
     )
 )
+
+df |> 
+  mutate(king_age = case_when(
+    is.na(king_age) | king_age %in% c("?", "–", "") ~ NA,
+    king_age == "50(?)" ~ "50",
+    TRUE ~ king_age
+  )) |> 
+  mutate(king_age = as.numeric(king_age)) |> 
+  mutate(consort_age = case_when(
+    is.na(consort_age) | consort_age %in% c("?", "–") ~ NA,
+    TRUE ~ consort_age
+  )) |> 
+  mutate(consort_age = as.numeric(consort_age)) |> 
+  mutate(
+    period = case_when(
+      year >= 856 & year <= 999 ~ "early middle age",
+      year >= 1000 & year <= 1299 ~ "high middle age",
+      year >= 1300 & year <= 1499 ~ "late middle age",
+      year >= 1500 & year <= 1699 ~ "renaissance",
+      year >= 1700 & year <= 1799 ~ "early modern period",
+      year >= 1800 & year <= 1947 ~ "industrial era",
+      TRUE ~ "NA"
+  )) |> 
+  filter(!is.na(year)) |> 
+  mutate(age_diff = (king_age - consort_age)) |> 
+  filter(!is.na(age_diff)) |> 
+  arrange(desc(age_diff))
 
 # visualise ----
 
@@ -112,7 +140,72 @@ df |>
   select(year, king_age, consort_age) |> 
   mutate(age_diff = (king_age - consort_age)) |> 
   filter(!is.na(age_diff)) |> 
-  arrange(year)
+  arrange(year) |> 
+  knitr::kable()
 
 # Communicate ----
 # ...
+
+df |> 
+  mutate(
+    king_age = as.numeric(replace(king_age, king_age %in% c("?", "–", "", "50(?)"), NA)),
+    consort_age = as.numeric(replace(consort_age, consort_age %in% c("?", "–", ""), NA)),
+    king = glue::glue("{king_name}, {king_age}"),
+    consort = glue::glue("{consort_name}, {consort_age}"),
+    period = case_when(
+      year >= 856 & year <= 999 ~ "early middle age",
+      year >= 1000 & year <= 1299 ~ "high middle age",
+      year >= 1300 & year <= 1499 ~ "late middle age",
+      year >= 1500 & year <= 1699 ~ "renaissance",
+      year >= 1700 & year <= 1799 ~ "early modern period",
+      year >= 1800 & year <= 1947 ~ "industrial era",
+      TRUE ~ NA_character_
+    ),
+    age_diff = king_age - consort_age
+  ) |> 
+  filter(!is.na(year) & !is.na(age_diff)) |> 
+  arrange(desc(age_diff)) |> 
+  ggplot(y = reorder(age_diff, age_diff)) +
+    geom_segment(aes(
+      x = age_diff,
+      xend = age_diff,
+      y = king_age,
+      yend = consort_age
+    ), color = "#3c3836") +
+    geom_point(
+      aes(x = age_diff, y = king_age),
+      color = '#3c3836',
+      alpha = 0.9,
+      size = 3.5
+    ) +
+    geom_point(
+      aes(x = age_diff, y = consort_age),
+      color = '#98971a',
+      alpha = 0.9,
+      size = 3.5
+    ) +
+    geom_text(
+      aes(x = age_diff, y = king_age, label = king),
+      hjust = 1.5,
+      size = 3,
+      family = 'Consolas'
+    ) +
+    geom_text(
+      aes(x = age_diff, y = consort_age, label = consort),
+      hjust = -0.5,
+      size = 3,
+      family = 'Consolas'
+    ) +
+    coord_flip() +
+    theme_minimal() +
+    theme(
+      legend.position = 'none',
+      text = element_text(family = 'Roboto Mono'),
+      plot.title = element_text(face = 'bold'),
+      plot.subtitle = element_text(family = 'Consolas'),
+      plot.caption = element_text(family = 'Consolas')
+    ) +
+    xlab("") +
+    ylab("") +
+    labs(title = "", subtitle = "", caption = "") +
+    facet_wrap(~ period)
