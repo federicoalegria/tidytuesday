33e087445ffcffed6146d6d4029d7817149d2cb3
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-10-29 14:00:19 -0600

    create tt2024_41.R

---
+++ 2024/41/tt2024_41.R
@@ -0,0 +1,53 @@
+# --- TIDYTUESDAY::YYYY§WW --- #
+# https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  styler,               # https://cran.r-project.org/web/packages/styler/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# Import ----
+df <-
+  fread(
+    'link.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.
+
+# Understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# tokenize
+# df |>
+#   unnest_tokens(output = word, input = variable) |>
+#   anti_join(stop_words, by = "word") |>
+#   group_by(word) |>
+#   summarise(n = n()) |>
+#   arrange(desc(n))
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# Communicate ----
+
+# ...
