ad5c3ab2ee3f51a8dc1a6d79de1a9428468cc8f0
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-11-20 16:02:33 -0600

    create tt2024_44.R

---
+++ 2024/44/tt2024_44.R
@@ -0,0 +1,59 @@
+# --- TIDYTUESDAY::YYYY§WW --- #
+# https://github.com/rfordatascience/tidytuesday/blob/master/data/2024/2024-10-29/readme.md
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  styler,               # https://cran.r-project.org/web/packages/styler/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# Import ----
+df00 <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-29/monster_movie_genres.csv'
+  ) |>
+  clean_names()
+
+df01 <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-29/monster_movies.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://t.ly/4rvTE
+
+# Understand ----
+
+# names
+df00 |> 
+  slice(0) |> 
+  glimpse()
+
+df01 |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df00 |>
+  glimpse() |>
+  skim()
+
+df01 |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# Communicate ----
+
+# ...
