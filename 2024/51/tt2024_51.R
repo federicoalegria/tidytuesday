8b7207f91550a9194cece2c423b65dfda37a4dc0
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2024-12-21 13:21:07 -0600

    create tt2024_51.R

---
+++ 2024/51/tt2024_51.R
@@ -0,0 +1,54 @@
+# --- TIDYTUESDAY::2024§51 --- #
+# https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  styler,               # https://cran.r-project.org/web/packages/styler/
+  tidytext,             # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# Import ----
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-12-17/spells.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://t.ly/0ZSx2
+
+# Understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# tokenize
+df |>
+  unnest_tokens(output = word, input = description) |>
+  anti_join(stop_words, by = "word") |>
+  group_by(word) |>
+  summarise(n = n()) |>
+  arrange(desc(n))
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# Communicate ----
+
+# ...
