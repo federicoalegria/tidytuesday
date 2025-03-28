ba8933037f7dc3e060475e5956b20c4ee3b14688
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-03-28 17:25:50 -0600

    create tt2509.R

---
+++ 2025/09/tt2509.R
@@ -0,0 +1,47 @@
+# --- tidytuesday::2509 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-03-04/readme.md
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  tidytext,             # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-04/longbeach.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-03-04/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# communicate ----
+
+df |>
+  filter(animal_type == "reptile") |> 
+  View()
