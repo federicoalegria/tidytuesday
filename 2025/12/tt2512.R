2a8598ba4788edfe7474ffc4f21d733b4b3ac71c
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-05-12 22:05:42 -0600

    create tt2512.R

---
+++ 2025/12/tt2512.R
@@ -0,0 +1,41 @@
+# --- tidytuesday::2512 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-03-25/readme.md
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  tidytext,             # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-25/report_words_clean.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-03-25/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# group
+df |> 
+  group_by(word) |> 
+  summarise(n = n()) |> 
+  arrange(desc(n))
