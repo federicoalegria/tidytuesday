21664f7f3a7e37e200e113f13a803fd5503f5395
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-05-29 21:45:06 -0600

    create tt2514.R

---
+++ 2025/14/tt2514.R
@@ -0,0 +1,48 @@
+# --- tidytuesday::2514 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-04-08/readme.md
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table,           # https://cran.r-project.org/web/packages/data.table/
+  janitor,              # https://cran.r-project.org/web/packages/janitor/
+  skimr,                # https://cran.r-project.org/web/packages/skimr/
+  tidytext,             # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-04-08/care_state.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-04-08/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# group
+df |> 
+  group_by(state) |> 
+  drop_na(score) |> 
+  summarise(sum = sum(score)) |> 
+  arrange(desc(sum))
+
+# visualise ----
+
+df |> 
+  ggplot(aes(x = score)) +
+  geom_density()
