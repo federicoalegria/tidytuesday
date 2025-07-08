1c1b38e515ddd220b0055a12ff2d5388b7f83f02
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-07-08 12:35:47 -0600

    update template.R

--- template.R
+++ template.R
@@ -1,21 +1,29 @@
 # --- tidytuesday::yyww --- #
-# https://github.com/rfordatascience/tidytuesday/tree/master/data/...
+# https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/...
 
-# lib/path
-.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.4", .libPaths()))
+# setup ----
+
+# library path
+.libPaths(c("~/.R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
 
 # packages
 pacman::p_load(
   data.table,           # https://cran.r-project.org/web/packages/data.table/
+  httr,                 # https://cran.r-project.org/web/packages/httr/
   janitor,              # https://cran.r-project.org/web/packages/janitor/
   skimr,                # https://cran.r-project.org/web/packages/skimr/
   tidytext,             # https://cran.r-project.org/web/packages/tidytext/
   tidyverse             # https://cran.r-project.org/web/packages/tidyverse/
 )
 
-# import ----
-
-# data
+# import
+## check size
+response <- HEAD('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-27/monsters.csv')
+bytes <- as.numeric(response$headers[["content-length"]])
+size_kb <- bytes / 1024
+size_mb <- size_kb / 1024
+cat(sprintf("df size: %.2f KB (%.2f MB)", size_kb, size_mb))
+## load data
 df <-
   fread(
     'link.csv'
