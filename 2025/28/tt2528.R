90f2dbe44103bddf9f34782076be83722104e9d2
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-08-27 22:13:35 -0600

    create tt2528.R

---
+++ 2025/28/tt2528.R
@@ -0,0 +1,48 @@
+# --- tidytuesday::yyww --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-07-15/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  styler, # https://cran.r-project.org/web/packages/styler/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-15/bl_funding.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-07-15/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# communicate ----
+
+# ...
