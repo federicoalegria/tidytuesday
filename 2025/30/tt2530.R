ce5c9e9e158f04464b99eb379c1bbc670202566b
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-09-26 18:40:48 -0600

    create tt2530.R

---
+++ 2025/30/tt2530.R
@@ -0,0 +1,67 @@
+# --- tidytuesday::2530 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-07-29/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  styler, # https://cran.r-project.org/web/packages/styler/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+dfa <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-29/movies.csv'
+  ) |>
+  clean_names()
+
+dfb <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-29/shows.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-07-29/readme.md
+
+df <- 
+  bind_rows(movie = dfa, show = dfb, .id = "type")
+
+rm(dfa, dfb)
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+df |> 
+  ggplot(aes(type)) +
+  stat_count(geom = "bar")
+
+# model ----
+
+# communicate ----
+
+# ...
