0603e2720c3f666787e1635f6b44b823377e02f1
Federico Alegría <116465926+federicoalegria@users.noreply.github.com>
2025-10-16 06:46:12 -0600

    create tt2532.R

---
+++ 2025/32/tt2532.R
@@ -0,0 +1,62 @@
+# --- tidytuesday::2532 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-08-12/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  styler, # https://cran.r-project.org/web/packages/styler/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+dfa <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-08-12/attribution_studies.csv'
+  ) |>
+  clean_names()
+
+dfb <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-08-12/attribution_studies_raw.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-08-12/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# communicate ----
+
+dfa |> 
+  group_by(cb_region) |> 
+  summarise(n = n())
+
+dfa |> 
+  filter(cb_region == "Latin America and the Caribbean") |> 
+  glimpse()
+
+# ...
